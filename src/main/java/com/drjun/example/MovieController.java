fbfda465aa259981f8d3132cee997809bda66dab
Jun JeongIl <dr.jun@sk.com>
2013-07-23 17:01:11 +0900

    reset on feature

--- src/main/java/com/drjun/example/MovieController.java
+++ src/main/java/com/drjun/example/MovieController.java
@@ -11,23 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MovieController {
 
 //  0 insert On dev
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-//  1 insert On feature
-
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
 
 	@RequestMapping(value="/{name}", method = RequestMethod.GET)
 	public String getMovie(@PathVariable String name, ModelMap model) {
