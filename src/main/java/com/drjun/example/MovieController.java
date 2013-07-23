97460896bd73f9386c405ea85fd37918cd918d97
Jun JeongIl <dr.jun@sk.com>
2013-07-23 17:20:50 +0900

    1 2 23 3

--- src/main/java/com/drjun/example/MovieController.java
+++ src/main/java/com/drjun/example/MovieController.java
@@ -11,17 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MovieController {
 
 //  0 insert On dev
-//  1 insert On feature
-
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
-//  2 insert On dev
 
 	@RequestMapping(value="/{name}", method = RequestMethod.GET)
-	public String getMovie333(@PathVariable String name, ModelMap model) {
+	public String getMovie(@PathVariable String name, ModelMap model) {
 
 		model.addAttribute("movie", name);
 		return "list";
